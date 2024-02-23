61a3a7fa5e0fcc3a63e8a8fc4c9fa506ba52daa0
Ghost <75737627+SebastianKoehler@users.noreply.github.com>
2024-02-23 23:56:24 +0100

    Inserting and experimenting with automatically created demo data from Bogus.

---
+++ Data/Faker.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using ProjectOrganizer.Models;
+
+namespace ProjectOrganizer.Data
+{
+    public class Faker
+    {
+        Faker<Project> projectModelFaker;
+
+        public Faker() 
+        {
+            Randomizer.Seed = new Random(123);
+
+            projectModelFaker = new Faker<Project>()
+                .RuleFor(project => project.Id, faker => faker.Random.Int(1,10000))
+                .RuleFor(project => project.Name, faker => faker.Name.JobTitle())
+                .RuleFor(project => project.Description, faker => faker.Name.JobDescriptor())
+                .RuleFor(project => project.Technologies, faker => faker.Name.JobType())
+                ;
+        }
+    }
+}
